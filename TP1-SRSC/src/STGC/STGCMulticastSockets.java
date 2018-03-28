153102e2d27848f309d823b30df14e8d5e716b5b
simao <simao.dolores@gmail.com>
2018-03-28 16:29:05 +0100

    Debug datagram size

--- TP1-SRSC/src/STGC/STGCMulticastSockets.java
+++ TP1-SRSC/src/STGC/STGCMulticastSockets.java
@@ -1,4 +1,5 @@
 package STGC;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
@@ -29,35 +30,33 @@ public class STGCMulticastSockets extends MulticastSocket{
 
 
 	public STGCMulticastSockets(int port) throws IOException {
-		
 		super(port);
 		try {
-	    KeyStore keyStore = KeyStore.getInstance("JCEKS");
-        // Keystore where symmetric keys are stored (type JCEKS)
-	    FileInputStream stream = new FileInputStream("mykeystore.jceks");
-	    // carregar keystore
-	    keyStore.load(stream, "projeto".toCharArray());
-	    
-	    PasswordProtection keyPassword = new PasswordProtection("srsc1718".toCharArray());
-	    PasswordProtection keyPassword2 = new PasswordProtection("srsc1718?yes!".toCharArray());
-	    //ler chave para cipher
-	    KeyStore.Entry entry = keyStore.getEntry("mykey1", keyPassword);
+		KeyStore keyStore = KeyStore.getInstance("JCEKS");
+		// Keystore where symmetric keys are stored (type JCEKS)
+		FileInputStream stream = new FileInputStream("mykeystore.jceks");
+		// carregar keystore
+		keyStore.load(stream, "projeto".toCharArray());
+
+		PasswordProtection keyPassword = new PasswordProtection("srsc1718".toCharArray());
+		PasswordProtection keyPassword2 = new PasswordProtection("srsc1718?yes!".toCharArray());
+		//ler chave para cipher
+		KeyStore.Entry entry = keyStore.getEntry("mykey1", keyPassword);
 		key = ((KeyStore.SecretKeyEntry) entry).getSecretKey();
 		//ler chave para mac
 		KeyStore.Entry entry2 = keyStore.getEntry("mykey3", keyPassword2);
 		macKey = ((KeyStore.SecretKeyEntry) entry2).getSecretKey();
-		
+
 		//ler o ficheiro de configurações
-	    readConfig();
-		}
-		catch(Exception e) {
+		readConfig();
+		}catch(Exception e) {
 			
 		}
-	    
+
 	}
 	public void send(DatagramPacket packet) {
 		byte[] cipherdata=null;
-		
+
 		try {
 			cipherdata = this.encrypt(packet.getData());
 		} catch (Exception e1) {
@@ -65,131 +64,133 @@ public class STGCMulticastSockets extends MulticastSocket{
 			e1.printStackTrace();
 		}
 
-	    DatagramPacket cipherPacket = new DatagramPacket(cipherdata, cipherdata.length, packet.getAddress(), 
-	    		packet.getPort());
-	    try {
-			super.send(cipherPacket);
+		packet.setData(cipherdata);
+		try {
+			super.send(packet);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	    System.out.println(Utils.toHex(cipherPacket.getData()));
+		//System.out.println(Utils.toHex(cipherPacket.getData()));
 	}
-	
+
 	public void receive(DatagramPacket packet) throws IOException {
+		int size = packet.getLength();
 		super.receive(packet);
-        byte[] receivedcipher= new byte[packet.getLength()];
-        System.arraycopy(packet.getData(), packet.getOffset(), receivedcipher, 0, packet.getLength());
-        System.out.println(packet.getLength());
-        
-        byte[] messagedata=null;
-        try {
+		byte[] buffer= new byte[size];
+		byte[] receivedcipher= new byte[packet.getLength()];
+		System.arraycopy(packet.getData(), packet.getOffset(), receivedcipher, 0, packet.getLength());
+
+		byte[] messagedata=null;
+		try {
 			messagedata=this.desencrypt(receivedcipher,packet.getLength());
-			System.out.println(messagedata.length);
-			
+
 		} catch (Exception e) {
-			System.out.println("ola");
+
 			e.printStackTrace();
 		}
-       // packet.setData(messagedata);
-        
+		System.arraycopy(messagedata, 0, buffer, 0, messagedata.length);
+
+		packet.setData(buffer);
+
 	}
 	public byte[] encrypt(byte[] message) throws Exception {
-		
-		cipher.init(Cipher.ENCRYPT_MODE, key, ivSpec);
-	        
-	        byte[] cipherText = new byte[cipher.getOutputSize(message.length + mac.getMacLength())];
 
-	        int ctLength = cipher.update(message, 0, message.length, cipherText, 0);
-	        
-	        // Parte do MAC
-	       
-	        mac.init(macKey);
-	        mac.update(message);
-	        
-	        ctLength += cipher.doFinal(mac.doFinal(), 0, mac.getMacLength(), cipherText, ctLength);
-	        
-	        byte[] headerAndPayload= createHeaderAndAddMessage(cipherText);
+		cipher.init(Cipher.ENCRYPT_MODE, key, ivSpec);
+
+		byte[] cipherText = new byte[cipher.getOutputSize(message.length + mac.getMacLength())];
+
+		int ctLength = cipher.update(message, 0, message.length, cipherText, 0);
+
+		// Parte do MAC
+
+		mac.init(macKey);
+		mac.update(message);
+
+		ctLength += cipher.doFinal(mac.doFinal(), 0, mac.getMacLength(), cipherText, ctLength);
+
+		byte[] headerAndPayload= createHeaderAndAddMessage(cipherText);
 		return headerAndPayload;
-		
+
 	}
 	public byte[] desencrypt(byte[] message,int lenght) throws Exception {
-		 cipher.init(Cipher.DECRYPT_MODE, key, ivSpec);
-		 	message=ReadMessage(message);
-		 	byte[] plainText = cipher.doFinal(message);
-	        int    messageLength = plainText.length - mac.getMacLength();
-	        
-	        // Verificaao Mac
+		cipher.init(Cipher.DECRYPT_MODE, key, ivSpec);
+		message=ReadMessage(message);
+		byte[] plainText = cipher.doFinal(message);
+		int    messageLength = plainText.length - mac.getMacLength();
 
-	        mac.init(macKey);
-	        mac.update(plainText, 0, messageLength);
-	        
-	        byte[] messageHash = new byte[mac.getMacLength()];
-	        System.arraycopy(plainText, messageLength, messageHash, 0, messageHash.length);
-	        
-	        byte[] messagePlain =new byte[lenght];
-	        System.arraycopy(plainText, 0, messagePlain, 0, messageLength);
-	        
-	       if(!MessageDigest.isEqual(mac.doFinal(), messageHash)){
-	    	   System.exit(1);
-	       }
-	        return messagePlain;
+		// Verificaao Mac
+
+		mac.init(macKey);
+		mac.update(plainText, 0, messageLength);
+
+		byte[] messageHash = new byte[mac.getMacLength()];
+		System.arraycopy(plainText, messageLength, messageHash, 0, messageHash.length);
+
+		byte[] messagePlain =new byte[lenght];
+		System.arraycopy(plainText, 0, messagePlain, 0, messageLength);
+
+		if(!MessageDigest.isEqual(mac.doFinal(), messageHash)){
+			System.exit(1);
+		}
+		return messagePlain;
 	}
-	
+
 	private void readConfig() throws Exception {
 		String macProvider=null;
 		String cipherProvider=null;
 		String cipherConfig=null;
 		String macCipher=null;
-	    Properties prop = new Properties();
-        // the configuration file name
-	    String fileName = "ciphersuite.conf";
-	    ClassLoader classLoader = STGCMulticastSockets.class.getClassLoader();
+		Properties prop = new Properties();
+		// the configuration file name
+		String fileName = "ciphersuite.conf";
+		ClassLoader classLoader = STGCMulticastSockets.class.getClassLoader();
 
-	            // Make sure that the configuration file exists
-	    URL res = Objects.requireNonNull(classLoader.getResource(fileName),
-	                "Can't find configuration file ciphersuite.conf");
+		// Make sure that the configuration file exists
+		URL res = Objects.requireNonNull(classLoader.getResource(fileName),
+				"Can't find configuration file ciphersuite.conf");
 
-	    InputStream is = new FileInputStream(res.getFile());
+		InputStream is = new FileInputStream(res.getFile());
 
-	            // load the properties file
-	    prop.load(is);
-	    
-	    macProvider=prop.getProperty("macProvider");
-	    macCipher=prop.getProperty("macCipher");
-	    cipherProvider= prop.getProperty("cipherProvider");
-	    cipherConfig=prop.getProperty("cipherConfig");
-	    
-	   //nao sei o que fazer com isto
+		// load the properties file
+		prop.load(is);
+
+		macProvider=prop.getProperty("macProvider");
+		macCipher=prop.getProperty("macCipher");
+		cipherProvider= prop.getProperty("cipherProvider");
+		cipherConfig=prop.getProperty("cipherConfig");
+
+		//nao sei o que fazer com isto
 		byte[] ivBytes= new byte[] {
 				0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
 				0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15 
 		};
+		
 		ivSpec = new IvParameterSpec(ivBytes);
 		mac = Mac.getInstance(macCipher, macProvider);
 		cipher = Cipher.getInstance(cipherConfig, cipherProvider);
 	}
 	private byte[] createHeaderAndAddMessage(byte[] payload) throws Exception {
-	    ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-	    DataOutputStream dataStream = new DataOutputStream(byteStream);
-	    
-	    byte[] version= new byte[] {0x11};
-	    byte[] space= new byte[] {0x00};
-	    byte[] type= new byte[] {0x01};// 0x01 para app 0x02 para o que seja
-	    dataStream.write(version);
-	    dataStream.write(space);
-	    dataStream.write(type);
-	    dataStream.write(space);
-	    dataStream.writeShort(payload.length);
-	    dataStream.write(payload);
-	    dataStream.close();
+		ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+		DataOutputStream dataStream = new DataOutputStream(byteStream);
 
-	    byte[] data = byteStream.toByteArray();
-	    return data;
+		byte[] version= new byte[] {0x11};
+		byte[] space= new byte[] {0x00};
+		byte[] type= new byte[] {0x01};// 0x01 para app 0x02 para o que seja
+		dataStream.write(version);
+		dataStream.write(space);
+		dataStream.write(type);
+		dataStream.write(space);
+		dataStream.writeShort(payload.length);
+		dataStream.write(payload);
+		dataStream.close();
+
+		byte[] data = byteStream.toByteArray();
+		return data;
 	}
 	private byte[] ReadMessage(byte[] messagedata) throws Exception {
 		DataInputStream istream = 
-		          new DataInputStream(new ByteArrayInputStream(messagedata));
+				new DataInputStream(new ByteArrayInputStream(messagedata));
 		byte version=istream.readByte();
 		istream.readByte();
 		byte type=istream.readByte();
@@ -198,7 +199,7 @@ public class STGCMulticastSockets extends MulticastSocket{
 		byte[] data= new byte[size];
 		istream.readFully(data);
 		return data;
-		
+
 	}
-	
+
 }
